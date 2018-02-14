cea029b3e7a2f12d2a178497fb89a2967b9c2323
Jennifer Le <j.le@Jennifers-MacBook-Air.local>
2018-02-13 18:20:02 -0800

    Prompt user action

--- liri.js
+++ liri.js
@@ -19,3 +19,42 @@ var client  = new Twitter(keys.twitter);
 // console.log('Spotify:', keys.spotify);
 // console.log('Twitter:', keys.twitter);
 // console.log('OMDB:', keys.omdb);
+
+
+/* 
+  High level: 
+    fn tweet(A)
+    fn song(A)
+    fn movie(A)
+    fn liriCommand() => runs fn song
+
+    fn promptUse()
+    fn promptLogic(A)
+*/
+
+
+function promptUser() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      message: 'Choose a LIRI command.',
+      choices: ['my-tweets','spotify-this-song', 'movie-this', 'do-what-it-says'],
+      name: 'userCommand'
+    }
+
+  ]).then(function(res) {
+    var userInput = res.userCommand;
+    // fs append user input to user log
+    if (userInput === 'my-tweets') {
+      getTweets(promptUser);
+    } else if (userInput === 'spotify-this-song') {
+      //
+    } else if (userInput === 'movie-this') {
+      //
+    } else if (userInput === 'do-what-it-says') {
+      //
+    }
+  });
+}
+
+promptUser();
