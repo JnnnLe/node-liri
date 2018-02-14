82804c2748859f0d3a3d144e8636f9fae27be8c2
Jennifer Le <j.le@Jennifers-MacBook-Air.local>
2018-02-13 18:17:54 -0800

    Npm dependecies + access to api keys

--- liri.js
+++ liri.js
@@ -1,23 +1,21 @@
 require("dotenv").config();
+
+//required key file 
 var keys = require('./keys.js');
 
-console.log("Spot: ", keys.spotify);
-console.log("Twit: ", keys.twitter);
+
+//npm dependencies 
+var fs = require('fs');
+var request  = require('request');
+var Spotify  = require('node-spotify-api');
+var Twitter  = require('twitter');
+var inquirer = require('inquirer');
 
 
-// var db = require('db')
-// db.connect({
-//   twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY,
-//   twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-//   twitterAccessToken: process.env.TWITTER_ACCESS_TOKEN_KEY,
-//   twitterTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-// });
+//Accessing api keys
+var spotify = new Spotify(keys.spotify);
+var client  = new Twitter(keys.twitter);
 
-// Attempting to import api keys from keys.js
-
-// npm dependencies 
-// var spotify = require('spotify');
-// var twitter = require('twitter');
-
-// var Spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
+// console.log('Spotify:', keys.spotify);
+// console.log('Twitter:', keys.twitter);
+// console.log('OMDB:', keys.omdb);
